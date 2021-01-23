6d0db6a12b892e65065a1445cb58bcc8077cbd5f
Christophe_Regouby <christophe.regouby@airbus.com>
2021-01-23 20:00:47 +0100

    add change config test

--- tests/testthat/test-hardhat-scenarios.R
+++ tests/testthat/test-hardhat-scenarios.R
@@ -1,12 +1,10 @@
 
-
 test_that("we can continue training with a additional fit", {
 
   data("ames", package = "modeldata")
 
   x <- ames[-which(names(ames) == "Sale_Price")]
   y <- ames$Sale_Price
-
   fit_1 <- tabnet_fit(x, y, epochs = 1)
   fit_2 <- tabnet_fit(x, y, fit_1, epochs = 1)
 
@@ -15,3 +13,16 @@ test_that("we can continue training with a additional fit", {
 
 })
 
+test_that("we can change the tabnet_options between training epoch", {
+
+  data("ames", package = "modeldata")
+
+  x <- ames[-which(names(ames) == "Sale_Price")]
+  y <- ames$Sale_Price
+  fit_1 <- tabnet_fit(x, y, epochs = 1)
+  fit_2 <- tabnet_fit(x, y, fit_2, epochs = 1, penalty = 0.003, learn_rate = 0.002)
+
+  expect_equal(fit_3$fit$config$epoch, 3)
+  expect_lte(mean(fit_2$fit$metrics[[2]]$train$loss), mean(fit_2$fit$metrics[[1]]$train$loss))
+
+})
