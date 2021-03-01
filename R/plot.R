a0d1a049951cd546e3ee49440c805828bf3c9e9d
Christophe_Regouby <christophe.regouby@airbus.com>
2021-03-01 23:11:51 +0100

    add tests for autoplot.tabnet_fit and autoplot.tabnet_pretrain. add autoplot.tabnet_explain

--- R/plot.R
+++ R/plot.R
@@ -22,7 +22,7 @@ autoplot.tabnet_fit <- function(object, ...) {
   collect_metrics <- tibble::enframe(object$fit$metrics,name = "epoch") %>%
     tidyr::unnest_longer(value,indices_to = "dataset") %>%
     tidyr::unnest_wider(value) %>%
-    dplyr::mutate(mean_loss = map_dbl(loss, mean),
+    dplyr::mutate(mean_loss = purrr::map_dbl(loss, mean),
            has_checkpoint = epoch %in% epoch_checkpointed_seq)
   checkpoints <- collect_metrics %>% dplyr::filter(has_checkpoint, dataset=="train")
   p <- ggplot(collect_metrics, aes(x=epoch, y=mean_loss, color=dataset)) +
@@ -34,3 +34,57 @@ autoplot.tabnet_fit <- function(object, ...) {
 
 #' @export
 autoplot.tabnet_pretrain <- autoplot.tabnet_fit
+
+#' Plot tabnet_explain interpretation
+#'
+#' @param object A `tabnet_explain` object as a result of [tabnet_explain()].
+#' @param type a character value. Either `"mask_agg"` the default, for per predictor
+#' mask importance aggregate,or `"steps"` for a plot at each mask step.
+#' @param ...  not used.
+#' @return A `ggplot2` object.
+#' @details
+#'  Plot the tabnet_explain object mask importance per variable along the predicted dataset.
+#'  `type="mask_agg"` output a single heatmap of mask aggregated values,
+#'  `type="mask_agg"` provides a facet plot of each mask along the model.
+#'
+#' @examples
+#' \donttest{
+#' data("ames", package = "modeldata")
+#' ames_fit <- tabnet_fit(Sale_Price ~. , data=ames, epoch=15)
+#' ames_explain <- tabnet_explain(ames_fit, ames)
+#' # Plot the model mask interpretation heatmap
+#' autoplot(ames_explain)
+#' }
+
+#'  @export
+autoplot.tabnet_explain <- function(object, type = c("mask_agg", "steps"), ...) {
+  type <- match.arg(type)
+
+  if (type == "steps") {
+    .data <- object$masks %>%
+      purrr::imap_dfr(~mutate(
+        .x,
+        step = sprintf("Step %d", .y),
+        rowname = row_number()
+      )) %>%
+      tidyr::pivot_longer(-c(rowname, step), names_to = "variable", values_to = "mask_agg") %>%
+      dplyr::group_by(step) %>%
+      dplyr::mutate(mask_agg = min(mask_agg , q995(mask_agg))) %>%
+      dplyr::ungroup()
+  } else {
+
+  .data <- object$M_explain %>%
+    dplyr::mutate(rowname = row_number()) %>%
+    tidyr::pivot_longer(-rowname, names_to = "variable", values_to = "mask_agg") %>%
+    dplyr::mutate(mask_agg = min(mask_agg , q995(mask_agg)),
+                  step = "mask_aggregate")
+  }
+
+  p <- ggplot(.data, aes(x = rowname, y = variable, fill = mask_agg)) +
+    geom_tile() +
+    scale_fill_viridis_c() +
+    facet_wrap(~step)
+  p
+}
+
+q995 <- function(x) quantile(x, probs = 0.995)
---
+++ tests/testthat/test-plots.R
@@ -0,0 +1,95 @@
+test_that("Autoplot with unsupervised training, w and wo valid_split", {
+
+  data("attrition", package = "modeldata")
+  ids <- sample(nrow(attrition), 256)
+
+  x <- attrition[ids,-which(names(attrition) == "Attrition")]
+  y <- attrition[ids,]$Attrition
+
+  tabnet_pretrain <- tabnet_pretrain(x, y, epochs = 12)
+  expect_error(
+    p <- autoplot(tabnet_pretrain),
+    regexp = NA
+  )
+
+  tabnet_pretrain <- tabnet_pretrain(x, y, epochs = 12, valid_split=0.3)
+  expect_error(
+    p <- autoplot(tabnet_pretrain),
+    regexp = NA
+  )
+
+})
+
+test_that("Autoplot with supervised training, w and wo valid_split", {
+
+  data("attrition", package = "modeldata")
+  ids <- sample(nrow(attrition), 256)
+
+  x <- attrition[ids,-which(names(attrition) == "Attrition")]
+  y <- attrition[ids,]$Attrition
+
+  tabnet_fit <- tabnet_fit(x, y, epochs = 12)
+  expect_error(
+    p <- autoplot(tabnet_fit),
+    regexp = NA
+  )
+
+  tabnet_fit <- tabnet_fit(x, y, epochs = 12, valid_split=0.3)
+  expect_error(
+    p <- autoplot(tabnet_fit),
+    regexp = NA
+  )
+
+})
+
+test_that("Autoplot without checkpoint", {
+
+  data("attrition", package = "modeldata")
+  ids <- sample(nrow(attrition), 256)
+
+  x <- attrition[ids,-which(names(attrition) == "Attrition")]
+  y <- attrition[ids,]$Attrition
+
+  tabnet_pretrain <- tabnet_pretrain(x, y, epochs = 3)
+  expect_error(
+    p <- autoplot(tabnet_pretrain),
+    regexp = NA
+  )
+
+  tabnet_pretrain <- tabnet_pretrain(x, y, epochs = 3, valid_split=0.3)
+  expect_error(
+    p <- autoplot(tabnet_pretrain),
+    regexp = NA
+  )
+
+  tabnet_fit <- tabnet_fit(x, y, epochs = 3)
+  expect_error(
+    p <- autoplot(tabnet_fit),
+    regexp = NA
+  )
+
+  tabnet_fit <- tabnet_fit(x, y, epochs = 3, valid_split=0.3)
+  expect_error(
+    p <- autoplot(tabnet_fit),
+    regexp = NA
+  )
+
+})
+
+test_that("Autoplot of pretrain then fit scenario", {
+
+  data("attrition", package = "modeldata")
+  ids <- sample(nrow(attrition), 256)
+
+  x <- attrition[ids,-which(names(attrition) == "Attrition")]
+  y <- attrition[ids,]$Attrition
+
+  tabnet_pretrain <- tabnet_pretrain(x, y, epochs = 12, valid_split=.2)
+  tabnet_fit <- tabnet_fit(x, y, tabnet_model=tabnet_pretrain, epochs = 12)
+
+  expect_error(
+    p <- autoplot(tabnet_fit),
+    regexp = NA
+  )
+
+})
