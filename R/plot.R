773a72a8387534a6e8f8dee221feff4217dea75d
Christophe_Regouby <christophe.regouby@airbus.com>
2021-03-02 13:43:29 +0100

    improve doc and example

--- R/plot.R
+++ R/plot.R
@@ -6,12 +6,13 @@
 #' @return A `ggplot2` object.
 #' @details
 #'  Plot the training loss along epochs, and validation loss along epochs if any.
-#'  A diamond symbol is added on epochs where model snapshot is available.
+#'  A diamond symbol is added on epochs where model snapshot is available, helping
+#'  the choice of `from_epoch` value for later model training resume.
 #'
 #' @examples
 #' \donttest{
 #' data("attrition", package = "modeldata")
-#' attrition_fit <- tabnet_fit(Attrition ~. , data=attrition, epoch=15)
+#' attrition_fit <- tabnet_fit(Attrition ~. , data=attrition, valid_split=0.2, epoch=15)
 #'
 #' # Plot the model loss over epochs
 #' autoplot(attrition_fit)
@@ -23,7 +24,8 @@ autoplot.tabnet_fit <- function(object, ...) {
     tidyr::unnest_longer(value,indices_to = "dataset") %>%
     tidyr::unnest_wider(value) %>%
     dplyr::mutate(mean_loss = purrr::map_dbl(loss, mean),
-           has_checkpoint = epoch %in% epoch_checkpointed_seq)
+           has_checkpoint = epoch %in% epoch_checkpointed_seq) %>%
+    dplyr::select(-loss)
   checkpoints <- collect_metrics %>% dplyr::filter(has_checkpoint, dataset=="train")
   p <- ggplot(collect_metrics, aes(x=epoch, y=mean_loss, color=dataset)) +
     geom_point(data = checkpoints, aes(x=epoch, y=mean_loss, color=dataset), shape=5) +
@@ -35,20 +37,22 @@ autoplot.tabnet_fit <- function(object, ...) {
 #' @export
 autoplot.tabnet_pretrain <- autoplot.tabnet_fit
 
-#' Plot tabnet_explain mask interpretation heatmap
+#' Plot tabnet_explain mask importance heatmap
 #'
 #' @param object A `tabnet_explain` object as a result of [tabnet_explain()].
-#' @param type a character value. Either `"mask_agg"` the default, for per predictor
-#'  mask importance aggregate,or `"steps"` for a plot at each mask step.
-#' @param quantile numerical value between 0 and 1. Provides quantile clipping on the
-#'  mask value
+#' @param type a character value. Either `"mask_agg"` the default, for a single
+#'  heatmap of aggregated mask importance per predictor along the dataset,
+#'   or `"steps"` for one heatmap at each mask step.
+#' @param quantile numerical value between 0 and 1. Provides quantile clipping of the
+#'  mask values
 #' @param ...  not used.
 #' @return A `ggplot2` object.
 #' @details
 #'  Plot the tabnet_explain object mask importance per variable along the predicted dataset.
 #'  `type="mask_agg"` output a single heatmap of mask aggregated values,
-#'  `type="mask_agg"` provides a facet plot of each mask along the model.
-#'  `quantile=.995` may be used for strong outlier filtering.
+#'  `type="steps"` provides a plot faceted along the `n_steps` mask present in the model.
+#'  `quantile=.995` may be used for strong outlier clipping, in order to better highlight
+#'  low values. `quantile=1`, the default, do not clip any values.
 #'
 #' @examples
 #' \donttest{
