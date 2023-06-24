8282bda968cf136ac16118dc66bc5ee8c5486d31
C. Regouby <christophe.regouby@free.fr>
2023-06-24 12:46:42 +0200

    add multi-outcome tests for formula and recipe

--- tests/testthat/test-hardhat_multi-outcome.R
+++ tests/testthat/test-hardhat_multi-outcome.R
@@ -1,5 +1,5 @@
 
-test_that("Training multi-output regression for data.frame", {
+test_that("Training multi-output regression from data.frame", {
 
   expect_no_error(
     fit <- tabnet_fit(x, data.frame(y = y, z = y + 1), epochs = 1)
@@ -12,7 +12,37 @@ test_that("Training multi-output regression for data.frame", {
 
 })
 
-test_that("Training multilabel classification for data.frame", {
+test_that("Training multi-output regression from formula", {
+
+  expect_no_error(
+    fit <- tabnet_fit(Sale_Price + Latitude + Longitude ~ ., small_ames, epochs = 1)
+  )
+
+  expect_no_error(
+    result <- predict(fit, ames)
+  )
+  expect_equal(ncol(result), 3)
+
+})
+
+test_that("Training multi-output regression from recipe", {
+
+  rec <- recipe(Sale_Price + Latitude + Longitude ~ ., data = small_ames) %>%
+    step_zv(all_predictors()) %>%
+    step_normalize(all_numeric(), -all_outcomes())
+
+  expect_no_error(
+    fit <- tabnet_fit(rec, small_ames, epochs = 1)
+  )
+
+  expect_no_error(
+    result <- predict(fit, ames)
+  )
+  expect_equal(ncol(result), 3)
+
+})
+
+test_that("Training multilabel classification from data.frame", {
 
   expect_no_error(
     fit <- tabnet_fit(attri_mult_x, data.frame(y = attriy, z = attriy, sat = attrix$JobSatisfaction),
@@ -40,7 +70,50 @@ test_that("Training multilabel classification for data.frame", {
 
 })
 
-test_that("Training multilabel classification for data.frame with validation split", {
+test_that("Training multilabel classification from formula", {
+
+  expect_no_error(
+    fit <- tabnet_fit(Attrition + JobSatisfaction ~ ., data = attrition[ids,],
+                      epochs = 1)
+  )
+
+  expect_no_error(
+    result <- predict(fit, attri_mult_x, type = "prob")
+  )
+
+  expect_equal(ncol(result), 2)
+  outcome_nlevels <- purrr::map_dbl(fit$blueprint$ptypes$outcomes, ~length(levels(.x)))
+  # we get back outcomes vars with a `.pred_` prefix
+  expect_equal(stringr::str_remove(names(result), ".pred_"), names(outcome_nlevels))
+
+  # result columns are tibbles of resp 2, 2, 4 columns
+  expect_true(all(purrr::map_lgl(result, tibble::is_tibble)))
+  expect_equal(purrr::map_dbl(result, ncol), outcome_nlevels, ignore_attr = TRUE)
+
+})
+
+test_that("Training multilabel classification from recipe", {
+
+  rec <- recipe(Attrition + JobSatisfaction ~ ., data = attrition[ids,]) %>%
+    step_zv(all_predictors()) %>%
+    step_normalize(all_numeric(), -all_outcomes())
+
+  expect_no_error(
+    fit <- tabnet_fit(rec, attrition[ids,], epochs = 1, valid_split = 0.25,
+                      verbose = TRUE)
+  )
+
+  expect_no_error(
+    result <- predict(fit, attrition)
+  )
+  expect_equal(ncol(result), 2)
+
+  outcome_nlevels <- purrr::map_dbl(fit$blueprint$ptypes$outcomes, ~length(levels(.x)))
+  expect_equal(stringr::str_remove(names(result), ".pred_class_"), names(outcome_nlevels))
+
+})
+
+test_that("Training multilabel classification from data.frame with validation split", {
 
   expect_no_error(
     fit <- tabnet_fit(attri_mult_x, data.frame(y=attriy, z=attriy, sat=attrix$JobSatisfaction),
@@ -76,6 +149,16 @@ test_that("Training multilabel mixed output fails with explicit error", {
   attri_multi_x <- attrix[-which(names(attrix) == "PercentSalaryHike")]
   expect_error(
     fit <- tabnet_fit(attri_multi_x, data.frame(y = attriy, hik = attrix$PercentSalaryHike), epochs = 1),
+    "Mixed multi-outcome type"
+  )
+  expect_error(
+    fit <- tabnet_fit(Attrition + PercentSalaryHike ~ ., data = attrition[ids,], epochs = 1),
+    "Mixed multi-outcome type"
+  )
+  rec <- recipe(Attrition + PercentSalaryHike ~ ., data = attrition[ids,]) %>%
+    step_normalize(all_numeric(), -all_outcomes())
+  expect_error(
+    fit <- tabnet_fit(rec, data = attrition[ids,], epochs = 1),
     "Mixed multi-outcome type"
   )
 })
