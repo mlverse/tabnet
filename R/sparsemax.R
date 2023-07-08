24bfbb580f81ef2b5d3dfce89fd1d3551d60a47e
C. Regouby <christophe.regouby@free.fr>
2023-07-08 15:32:49 +0200

    autoimport and simplify sparsemax.R

--- R/sparsemax.R
+++ R/sparsemax.R
@@ -1,16 +1,20 @@
 
 # credits to Yandex https://github.com/Qwicen/node/blob/master/lib/nn_utils.py
+#' @importFrom purrr transpose
+#' @importFrom tibble view
+#' @importFrom torch torch_arange
 .make_ix_like <- function(input, dim = 1) {
   d <- input$size(dim)
-  rho <- torch::torch_arange(start = 1, end = d, device = input$device,
+  rho <- torch_arange(start = 1, end = d, device = input$device,
                              dtype = input$dtype)
   view <- rep(1, input$dim())
   view[1] <- -1
   rho$view(view)$transpose(1, dim)
 }
 
+#' @importFrom torch torch_sort
 .threshold_and_support <- function(input, dim) {
-  input_srt <- torch::torch_sort(input, descending=TRUE, dim=dim)[[1]]
+  input_srt <- torch_sort(input, descending=TRUE, dim=dim)[[1]]
   input_cumsum <- input_srt$cumsum(dim) - 1
   rhos <- .make_ix_like(input, dim)
   support <- rhos * input_srt > input_cumsum
@@ -24,12 +28,13 @@
   )
 }
 
+#' @importFrom torch autograd_function torch_clamp torch_where
 sparsemax_function <- torch::autograd_function(
   forward = function(ctx, input, dim = -1) {
     max_val <- input$max(dim=dim, keepdim=TRUE)[[1]]
     input$sub_(max_val) # same numerical stability trick as for softmax
     tau_supp_size = .threshold_and_support(input, dim=dim)
-    output <- torch::torch_clamp(input - tau_supp_size[[1]], min=0)
+    output <- torch_clamp(input - tau_supp_size[[1]], min=0)
     ctx$save_for_backward(supp_size = tau_supp_size[[2]], output = output, dim = dim)
     output
   },
@@ -43,7 +48,7 @@ sparsemax_function <- torch::autograd_function(
 
     v_hat <- grad_input$sum(dim=dim) / saved$supp_size$to(dtype = saved$output$dtype)$squeeze()
     v_hat <- v_hat$unsqueeze(dim)
-    grad_input <- torch::torch_where(saved$output != 0, grad_input - v_hat, grad_input)
+    grad_input <- torch_where(saved$output != 0, grad_input - v_hat, grad_input)
 
     list(
       input = grad_input,
@@ -52,6 +57,7 @@ sparsemax_function <- torch::autograd_function(
   }
 )
 
+#' @importFrom torch nn_module
 sparsemax <- torch::nn_module(
   "sparsemax",
   initialize = function(dim = -1) {
@@ -63,16 +69,17 @@ sparsemax <- torch::nn_module(
 )
 
 
+#' @importFrom torch torch_clamp torch_sort torch_sqrt
 .entmax_threshold_and_support <- function(input, dim) {
-  input_srt <- torch::torch_sort(input, descending=TRUE, dim=dim)[[1]]
+  input_srt <- torch_sort(input, descending=TRUE, dim=dim)[[1]]
   rho <- .make_ix_like(input, dim)
   mean <- input_srt$cumsum(dim) / rho
   mean_sq <- (input_srt ^ 2)$cumsum(dim) / rho
   ss <- rho * (mean_sq -mean ^ 2)
   delta <- (1 - ss) / rho
 
-  delta_nz <- torch::torch_clamp(delta, 0)
-  tau <- mean - torch::torch_sqrt(delta_nz)
+  delta_nz <- torch_clamp(delta, 0)
+  tau <- mean - torch_sqrt(delta_nz)
 
   support_size <- (tau <= input_srt)$sum(dim)$unsqueeze(dim)
   tau_star <- tau$gather(dim, support_size)
@@ -82,6 +89,7 @@ sparsemax <- torch::nn_module(
   )
 }
 
+#' @importFrom torch autograd_function torch_clamp
 entmax_function <- torch::autograd_function(
   forward = function(ctx, input, dim = -1) {
     max_val <- input$max(dim=dim, keepdim=TRUE)[[1]]
@@ -89,7 +97,7 @@ entmax_function <- torch::autograd_function(
     input <- input / 2
 
     tau_supp <- .entmax_threshold_and_support(input, dim=dim)
-    output <- torch::torch_clamp(input - tau_supp[[1]], min=0) ^ 2
+    output <- torch_clamp(input - tau_supp[[1]], min=0) ^ 2
     ctx$save_for_backward(supp_size = tau_supp[[2]], output = output, dim = dim)
     output
   },
@@ -112,6 +120,7 @@ entmax_function <- torch::autograd_function(
   }
 )
 
+#' @importFrom torch nn_module
 entmax <- torch::nn_module(
   "entmax",
   initialize = function(dim = -1) {
