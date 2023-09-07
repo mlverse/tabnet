071291cc773e21b7604a82c3b2060f355d82019c
cregouby <cregouby@users.noreply.github.com>
2023-09-07 16:22:17 +0200

    add decoder GLU layers specific number of blocks (#129)
    * add decoder GRU layers specific number of parameters

--- R/model.R
+++ R/model.R
@@ -83,12 +83,16 @@ resolve_data <- function(x, y) {
 #' @param learn_rate initial learning rate for the optimizer.
 #' @param optimizer the optimization method. currently only 'adam' is supported,
 #'   you can also pass any torch optimizer function.
-#' @param valid_split (float) The fraction of the dataset used for validation.
+#' @param valid_split (`[0, 1)`) The fraction of the dataset used for validation.
 #'   (default = 0 means no split)
-#' @param num_independent Number of independent Gated Linear Units layers at each step.
+#' @param num_independent Number of independent Gated Linear Units layers at each step of the encoder.
 #'   Usual values range from 1 to 5.
-#' @param num_shared Number of shared Gated Linear Units at each step Usual values
-#'   range from 1 to 5
+#' @param num_shared Number of shared Gated Linear Units at each step of the encoder. Usual values
+#'    at each step of the decoder. range from 1 to 5
+#' @param num_independent_decoder For pretraining, number of independent Gated Linear Units layers
+#'   Usual values range from 1 to 5.
+#' @param num_shared_decoder For pretraining, number of shared Gated Linear Units at each step of the
+#'    decoder. Usual values range from 1 to 5.
 #' @param verbose (logical) Whether to print progress and loss values during
 #'   training.
 #' @param lr_scheduler if `NULL`, no learning rate decay is used. If "step"
@@ -101,7 +105,9 @@ resolve_data <- function(x, y) {
 #'   or `NULL`.
 #' @param step_size the learning rate scheduler step size. Unused if
 #'   `lr_scheduler` is a `torch::lr_scheduler` or `NULL`.
-#' @param cat_emb_dim Embedding size for categorical features (default=1)
+#' @param cat_emb_dim Size of the embedding of categorical features. If int, all categorical
+#'   features will have same embedding size, if list of int, every corresponding feature will have
+#'   specific embedding size.
 #' @param momentum Momentum for batch normalization, typically ranges from 0.01
 #'   to 0.4 (default=0.02)
 #' @param pretraining_ratio Ratio of features to mask for reconstruction during
@@ -147,6 +153,8 @@ tabnet_config <- function(batch_size = 1024^2,
                           cat_emb_dim = 1,
                           num_independent = 2,
                           num_shared = 2,
+                          num_independent_decoder = 1,
+                          num_shared_decoder = 1,
                           momentum = 0.02,
                           pretraining_ratio = 0.5,
                           verbose = FALSE,
@@ -190,6 +198,8 @@ tabnet_config <- function(batch_size = 1024^2,
     cat_emb_dim = cat_emb_dim,
     n_independent = num_independent,
     n_shared = num_shared,
+    n_independent_decoder = num_independent_decoder,
+    n_shared_decoder = num_shared_decoder,
     momentum = momentum,
     pretraining_ratio = pretraining_ratio,
     verbose = verbose,
@@ -198,7 +208,7 @@ tabnet_config <- function(batch_size = 1024^2,
     early_stopping_monitor = resolve_early_stop_monitor(early_stopping_monitor, valid_split),
     early_stopping_tolerance = early_stopping_tolerance,
     early_stopping_patience = early_stopping_patience,
-    early_stopping = !(early_stopping_tolerance==0 || early_stopping_patience==0),
+    early_stopping = !(early_stopping_tolerance == 0 || early_stopping_patience == 0),
     num_workers = num_workers,
     skip_importance = skip_importance
   )
--- tests/testthat/test-pretraining.R
+++ tests/testthat/test-pretraining.R
@@ -2,9 +2,8 @@ test_that("transpose_metrics is not adding an unnamed entry on top of the list",
 
   metrics <- list(loss = 1, loss = 2, loss = 3, loss = 4)
 
-  expect_error(
-    tabnet:::transpose_metrics(metrics),
-    regexp = NA
+  expect_no_error(
+    tabnet:::transpose_metrics(metrics)
   )
 
   expect_equal(
@@ -16,23 +15,20 @@ test_that("transpose_metrics is not adding an unnamed entry on top of the list",
 
 test_that("Unsupervised training with default config, data.frame and formula", {
 
-  expect_error(
-    fit <- tabnet_pretrain(x, y, epochs = 1),
-    regexp = NA
+  expect_no_error(
+    fit <- tabnet_pretrain(x, y, epochs = 1)
   )
 
-  expect_error(
-    fit <- tabnet_pretrain(Sale_Price ~ ., data = ames, epochs = 1),
-    regexp = NA
+  expect_no_error(
+    fit <- tabnet_pretrain(Sale_Price ~ ., data = ames, epochs = 1)
   )
 
 })
 
 test_that("Unsupervised training with pretraining_ratio", {
 
-  expect_error(
-    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, pretraining_ratio=0.2),
-    regexp = NA
+  expect_no_error(
+    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, pretraining_ratio=0.2)
   )
 
 })
@@ -64,14 +60,12 @@ test_that("errors when using an argument that do not exist", {
 
 test_that("works with validation split", {
 
-  expect_error(
-    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, valid_split = 0.2),
-    regexp = NA
+  expect_no_error(
+    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, valid_split = 0.2)
   )
 
-  expect_error(
-    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, valid_split = 0.2, verbose = TRUE),
-    regexp = NA
+  expect_no_error(
+    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, valid_split = 0.2, verbose = TRUE)
   )
 
 })
@@ -80,9 +74,8 @@ test_that("works with categorical embedding dimension as list", {
 
   config <- tabnet_config(cat_emb_dim=c(1,1,2,2,1,1,1,2,1,1,1,2,2,2))
 
-  expect_error(
-    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, valid_split = 0.2, config=config),
-    regexp = NA
+  expect_no_error(
+    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1, valid_split = 0.2, config=config)
   )
 })
 
@@ -101,58 +94,52 @@ test_that("can train from a recipe", {
   rec <- recipe(Attrition ~ ., data = attrition) %>%
     step_normalize(all_numeric(), -all_outcomes())
 
-  expect_error(
-    pretrain <- tabnet_pretrain(rec, attrition, epochs = 1, verbose = TRUE),
-    regexp = NA
+  expect_no_error(
+    pretrain <- tabnet_pretrain(rec, attrition, epochs = 1, verbose = TRUE)
   )
 
 })
 
 test_that("lr scheduler step works", {
 
-  expect_error(
+  expect_no_error(
     fit <- tabnet_pretrain(x, y, epochs = 3, lr_scheduler = "step",
-                      lr_decay = 0.1, step_size = 1),
-    regexp = NA
+                           lr_decay = 0.1, step_size = 1)
   )
 
   sc_fn <- function(optimizer) {
     torch::lr_step(optimizer, step_size = 1, gamma = 0.1)
   }
 
-  expect_error(
+  expect_no_error(
     fit <- tabnet_pretrain(x, y, epochs = 3, lr_scheduler = sc_fn,
-                      lr_decay = 0.1, step_size = 1),
-    regexp = NA
+                           lr_decay = 0.1, step_size = 1)
   )
 
 })
 
 test_that("lr scheduler reduce_on_plateau works", {
 
-  expect_error(
+  expect_no_error(
     fit <- tabnet_pretrain(x, y, epochs = 3, lr_scheduler = "reduce_on_plateau",
-                           lr_decay = 0.1, step_size = 1),
-    regexp = NA
+                           lr_decay = 0.1, step_size = 1)
   )
 
   sc_fn <- function(optimizer) {
     torch::lr_reduce_on_plateau(optimizer, factor = 0.1, patience = 10)
   }
 
-  expect_error(
+  expect_no_error(
     fit <- tabnet_pretrain(x, y, epochs = 3, lr_scheduler = sc_fn,
-                           lr_decay = 0.1, step_size = 1),
-    regexp = NA
+                           lr_decay = 0.1, step_size = 1)
   )
 
 })
 
 test_that("checkpoints works", {
 
-  expect_error(
-    pretrain <- tabnet_pretrain(x, y, epochs = 3, checkpoint_epochs = 1),
-    regexp = NA
+  expect_no_error(
+    pretrain <- tabnet_pretrain(x, y, epochs = 3, checkpoint_epochs = 1)
   )
 
   expect_length( pretrain$fit$checkpoints, 3  )
@@ -167,13 +154,37 @@ test_that("print module works", {
   testthat::skip_on_os("linux")
   testthat::skip_on_os("windows")
 
-  expect_error(
-    fit <- tabnet_pretrain(x, y, epochs = 1),
-    regexp = NA
+  expect_no_error(
+    fit <- tabnet_pretrain(x, y, epochs = 1)
   )
 
   withr::with_options(new = c(cli.width = 50),
                       expect_snapshot_output(fit))
 
+})
+
+test_that("num_independent_decoder and num_shared_decoder change the network number of parameters", {
+
+  expect_no_error(
+    pretrain <- tabnet_pretrain(attrix, attriy, epochs = 1,
+                                num_independent_decoder = 3, num_shared_decoder = 2)
+  )
+  expect_gt( torch:::get_parameter_count(pretrain$fit$network),
+             torch:::get_parameter_count(attr_pretrained$fit$network)
+  )
+})
+
+test_that("num_independent_decoder and num_shared_decoder do not change the network number of parameters for fit", {
+
+  expect_no_error(
+    config <- tabnet_config(epochs = 1,
+                            num_independent_decoder = 3, num_shared_decoder = 2)
+  )
+  expect_no_error(
+    attr_fit <- tabnet_fit(attrix, attriy, config = config)
+  )
+  expect_equal( torch:::get_parameter_count(attr_fit$fit$network),
+                torch:::get_parameter_count(attr_fitted$fit$network)
+  )
 })
 
