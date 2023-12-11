3dc147fb05ae9725c18847745e5a1a15298518c0
C. Regouby <christophe.regouby@free.fr>
2023-12-11 14:43:49 +0100

    add MB_WLU

--- R/activation.R
+++ R/activation.R
@@ -1,27 +1,42 @@
-#' Applies the Multibranch Weighted Linear Unit (MB-WLU) function, element-wise.
+#' Multibranch Weighted Linear Unit (MB-WLU) nn module.
 #'
 #' @param input (N,*) tensor, where * means, any number of additional
 #'   dimensions
-#' @param alpha the weight of ELU activation component.
-#' @param beta the weight of PRELU activation component.
-#' @param gamma the weight of SILU activation component.
+#' @param alpha (float) the weight of ELU activation component.
+#' @param beta (float) the weight of PRELU activation component.
+#' @param gamma (float) the weight of SILU activation component.
+#' @param init (float): the initial value of \eqn{a} of PRELU. Default: 0.25.
 #'
 #' @examplesIf torch::torch_is_installed()
 #' x <- torch::torch_randn(2, 2)
-#' y <- nn_mnwlu(x, alpha = 0.6, beta = 0.2, gamma = 0.2)
-#' z <- nn_mnwlu(x)
+#' my_mbwlu <- nn_mbwlu(alpha = 0.6, beta = 0.2, gamma = 0.2)
+#' mbwlu <- nn_mbwlu()
+#' y <- my_mbwlu(x)
+#' z <- mbwlu(x)
 #' torch::torch_equal(y, z)
 #' @export
 nn_mbwlu <- torch::nn_module(
   "multibranch Weighted Linear Unit",
-  initialize = function(alpha = 0.6, beta = 0.2, gamma = 0.2) {
+  initialize = function(alpha = 0.6, beta = 0.2, gamma = 0.2, init = 0.25) {
     self$alpha <- alpha
     self$beta <- beta
     self$gamma <- gamma
+    self$init <- init
   },
   forward = function(input) {
-    self$alpha * torch::nn_elu(input, inplace=FALSE) +
-      self$beta * torch::nn_prelu(input, inplace=FALSE) +
-      self$gamma * torch::nn_silu(input)
+    nnf_mbwlu(input, self$alpha, self$beta, self$gamma, self$init)
   }
 )
+
+#' Applies the Multibranch Weighted Linear Unit (MB-WLU) function, element_wise.
+#' See [nn_mbwlu()] for more information.
+#' @seealso [nn_mbwlu()].
+#' @inheritParams nn_mbwlu
+#' @export
+#' @rdname nn_mbwlu
+nnf_mbwlu <- function(input, alpha, beta, gamma,  init) {
+    alpha * torch::nnf_elu(input) +
+      beta * torch::nnf_prelu(input, init) +
+      gamma * torch::nnf_silu(input)
+
+}
--- tests/testthat/test-activation.R
+++ tests/testthat/test-activation.R
@@ -1,7 +1,7 @@
-test_that("multibranch_weighted_linear_unit works", {
+test_that("multibranch_weighted_linear_unit nn_module works", {
   mb_wlu <- nn_mbwlu()
-  input <- torch_tensor(c(-1.0, 0.0, 1.0))
-  expected_output <- torch_tensor(c(-0.26894142, 0.0, 0.73105858))
+  input <- torch::torch_tensor(c(-1.0, 0.0, 1.0))
+  expected_output <- torch::torch_tensor(c(-0.4830606, 0.0, 0.9462118))
 
   expect_equal_to_tensor(mb_wlu(input), expected_output)
 
