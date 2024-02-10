e820c7b454eb62bbe7c1910a18947974ac0b5fdb
C. Regouby <christophe.regouby@free.fr>
2024-02-10 16:19:02 +0100

    fix #149

--- R/plot.R
+++ R/plot.R
@@ -21,35 +21,34 @@
 #'
 autoplot.tabnet_fit <- function(object, ...) {
 
-  epoch_checkpointed_seq <- seq_along(object$fit$checkpoints) * object$fit$config$checkpoint_epochs
-
-  collect_metrics <- tibble::enframe(object$fit$metrics,name = "epoch") %>%
-    tidyr::unnest_longer(value,indices_to = "dataset") %>%
+  collect_metrics <- tibble::enframe(object$fit$metrics, name = "epoch") %>%
     tidyr::unnest_wider(value) %>%
-    # drop entries from pretrain that have missing `dataset`
-    tidyr::drop_na(dataset) %>%
-    tidyr::pivot_wider(values_from = loss, names_from = dataset) %>%
-    # remove the valid col if all NAs to prevent ggplot warnings
+    dplyr::mutate_if(is.list, ~purrr::map_dbl(.x, mean)) %>%
     dplyr::select_if(function(x) {!all(is.na(x))} ) %>%
-    tidyr::pivot_longer(cols = !epoch, names_to = "dataset", values_to = "loss") %>%
-    # add checkpoints
-    dplyr::mutate(mean_loss = purrr::map_dbl(loss, mean),
-           has_checkpoint = epoch %in% (epoch_checkpointed_seq + min(epoch, na.rm=TRUE) - 1)) %>%
-    dplyr::select(-loss)
+    tidyr::pivot_longer(cols = !dplyr::matches("epoch|checkpoint"),
+                        names_to = "dataset", values_to = "loss")
 
-  checkpoints <- collect_metrics %>%
-    dplyr::filter(has_checkpoint, dataset=="train") %>%
-    dplyr::mutate(size=2)
-  p <- ggplot2::ggplot(collect_metrics, ggplot2::aes(x=epoch, y=mean_loss, color=dataset)) +
+
+  p <- ggplot2::ggplot(collect_metrics, ggplot2::aes(x = epoch, y = loss, color = dataset)) +
     ggplot2::geom_line() +
-    ggplot2::geom_point(data = checkpoints, ggplot2::aes(x=epoch, y=mean_loss, color=dataset, size = .data$size ) ) +
     ggplot2::scale_y_log10() +
-    ggplot2::guides(colour = ggplot2::guide_legend("Dataset", order=1, override.aes = list(size=1.5, shape=" ")),
-           size= ggplot2::guide_legend("has checkpoint", order=2, override.aes = list(size=3, color="#F8766D"), label.theme = ggplot2::element_text(colour = "#FFFFFF"))) +
+    ggplot2::guides(colour = ggplot2::guide_legend("Dataset", order=1, override.aes = list(size = 1.7, shape = " ")),
+                    size = ggplot2::guide_legend("has checkpoint", order = 2, override.aes = list(size = 3, color = "#F8766D"),
+                                                 label.theme = ggplot2::element_text(colour = "#FFFFFF"))) +
     ggplot2::theme(legend.position = "bottom") +
     ggplot2::labs(y="Mean loss (log scale)")
-  p
+
+  if ("checkpoint" %in% names(collect_metrics)) {
+    checkpoints <- collect_metrics %>%
+      dplyr::filter(checkpoint == TRUE, dataset == "train") %>%
+      dplyr::select(-checkpoint) %>%
+      dplyr::mutate(size = 2)
+    p +
+      ggplot2::geom_point(data = checkpoints, ggplot2::aes(x = epoch, y = loss, color = dataset, size = .data$size ))
+  } else {
+    p
   }
+}
 
 #' @rdname autoplot.tabnet_fit
 autoplot.tabnet_pretrain <- autoplot.tabnet_fit
