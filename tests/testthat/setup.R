2ed0e6888f6b44024366f38bb931bd8160a963c6
C. Regouby <christophe.regouby@free.fr>
2024-06-16 09:27:47 +0200

    fix 'cleanup not found'

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -46,4 +46,4 @@ attrition_tree <- attrition %>%
   data.tree::as.Node()
 
 # Run after all tests
-withr::defer(cleanup(), teardown_env())
+withr::defer(globals::cleanup(), testthat::teardown_env())
