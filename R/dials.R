9732d021972d328d558cfe04be974de36e7fcad5
C. Regouby <christophe.regouby@free.fr>
2024-07-27 17:07:44 +0200

    properly manage dials parameters in parsnip, rename 2 `tabnet()` params to `rate_decay` and `rate_step_size` add tests for that

--- R/dials.R
+++ R/dials.R
@@ -17,20 +17,6 @@ check_dials <- function() {
 #' @rdname tabnet_params
 #' @return A `dials` parameter to be used when tuning TabNet models.
 #' @export
-decision_width <- function(range = c(8L, 64L), trans = NULL) {
-  check_dials()
-  dials::new_quant_param(
-    type = "integer",
-    range = range,
-    inclusive = c(TRUE, TRUE),
-    trans = trans,
-    label = c(decision_width = "Width of the decision prediction layer"),
-    finalize = NULL
-  )
-}
-
-#' @rdname tabnet_params
-#' @export
 attention_width <- function(range = c(8L, 64L), trans = NULL) {
   check_dials()
   dials::new_quant_param(
@@ -45,17 +31,18 @@ attention_width <- function(range = c(8L, 64L), trans = NULL) {
 
 #' @rdname tabnet_params
 #' @export
-num_steps <- function(range = c(3L, 10L), trans = NULL) {
+decision_width <- function(range = c(8L, 64L), trans = NULL) {
   check_dials()
   dials::new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
     trans = trans,
-    label = c(num_steps = "Number of steps in the architecture"),
+    label = c(decision_width = "Width of the decision prediction layer"),
     finalize = NULL
   )
 }
+
 
 #' @rdname tabnet_params
 #' @export
@@ -67,6 +54,33 @@ feature_reusage <- function(range = c(1, 2), trans = NULL) {
     inclusive = c(TRUE, TRUE),
     trans = trans,
     label = c(feature_reusage = "Coefficient for feature reusage in the masks"),
+    finalize = NULL
+  )
+}
+
+#' @rdname tabnet_params
+#' @export
+momentum <- function(range = c(0.01, 0.4), trans = NULL) {
+  check_dials()
+  dials::new_quant_param(
+    type = "double",
+    range = range,
+    inclusive = c(TRUE, TRUE),
+    trans = trans,
+    label = c(momentum = "Momentum for batch normalization"),
+    finalize = NULL
+  )
+}
+
+
+#' @rdname tabnet_params
+#' @export
+mask_type <- function(values = c("sparsemax", "entmax")) {
+  check_dials()
+  dials::new_qual_param(
+    type = "character",
+    values = values,
+    label = c(mask_type = "Final layer of feature selector, either sparsemax or entmax"),
     finalize = NULL
   )
 }
@@ -101,28 +115,69 @@ num_shared <- function(range = c(1L, 5L), trans = NULL) {
 
 #' @rdname tabnet_params
 #' @export
-momentum <- function(range = c(0.01, 0.4), trans = NULL) {
+num_steps <- function(range = c(3L, 10L), trans = NULL) {
   check_dials()
   dials::new_quant_param(
-    type = "double",
+    type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
     trans = trans,
-    label = c(momentum = "Momentum for batch normalization"),
+    label = c(num_steps = "Number of steps in the architecture"),
     finalize = NULL
   )
 }
 
-
-#' @rdname tabnet_params
+#' @noRd
 #' @export
-mask_type <- function(values = c("sparsemax", "entmax")) {
+cat_emb_dim <- function(range = NULL, trans = NULL) {
   check_dials()
-  dials::new_qual_param(
-    type = "character",
-    values = values,
-    label = c(mask_type = "Final layer of feature selector, either sparsemax or entmax"),
-    finalize = NULL
-  )
+  cli::cli_abort("{.var cat_emb_dim} cannot be used as a {.fun tune} parameter yet.")
 }
 
+#' @noRd
+#' @export
+checkpoint_epochs <- cat_emb_dim
+
+#' @noRd
+#' @export
+drop_last <- cat_emb_dim
+
+#' @noRd
+#' @export
+encoder_activation <- cat_emb_dim
+
+#' @noRd
+#' @export
+lr_scheduler <- cat_emb_dim
+
+#' @noRd
+#' @export
+mlp_activation <- cat_emb_dim
+
+#' @noRd
+#' @export
+mlp_hidden_multiplier <- cat_emb_dim
+
+#' @noRd
+#' @export
+num_independent_decoder <- cat_emb_dim
+
+#' @noRd
+#' @export
+num_shared_decoder <- cat_emb_dim
+
+#' @noRd
+#' @export
+optimizer <- cat_emb_dim
+
+#' @noRd
+#' @export
+penalty <- cat_emb_dim
+
+#' @noRd
+#' @export
+verbose <- cat_emb_dim
+
+#' @noRd
+#' @export
+virtual_batch_size <- cat_emb_dim
