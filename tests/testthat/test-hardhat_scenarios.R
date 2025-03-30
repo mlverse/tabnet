7158b7d5203db9acd9ffd4b0875da67e10b14c37
C. Regouby <christophe.regouby@free.fr>
2025-03-30 19:20:13 +0200

    make test conditional to version

--- tests/testthat/test-hardhat_scenarios.R
+++ tests/testthat/test-hardhat_scenarios.R
@@ -81,6 +81,9 @@ test_that("Supervised training can continue unsupervised training, with or wo fr
 })
 
 test_that("Supervised training can continue unsupervised training, with a Libtorch optimizer", {
+  # `optim_ignite_adamw` requires a minimum torch version
+  torch_has_optim_ignite = compareVersion(as.character(packageVersion("torch")), "0.14.0") >= 0
+  testthat::skip_if(!torch_has_optim_ignite)
 
   expect_no_error(
     tabnet_fit(x, y, tabnet_model = ames_pretrain, epoch = 1, config = tabnet_config(
