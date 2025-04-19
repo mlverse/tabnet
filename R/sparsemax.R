ce79490fed1a03e176aa4eb83c1d5634956d5ae2
C. Regouby <christophe.regouby@free.fr>
2025-04-20 00:04:41 +0200

    add relu1.5 (WiP) refactor entmax

--- R/sparsemax.R
+++ R/sparsemax.R
@@ -9,11 +9,21 @@
   rho$view(view)$transpose(1, dim)
 }
 
+.roll_last <- function(input, dim = 1) {
+  if (dim == -1) {
+    return(input)
+  } else if (dim < 0) {
+    dim <- input$dim() - dim
+  }
+  perm <- c(which(1:input$dim() != dim), dim)
+  return(input$permute(perm))
+}
+
 .threshold_and_support <- function(input, dim) {
-  input_srt <- torch::torch_sort(input, descending=TRUE, dim=dim)[[1]]
-  input_cumsum <- input_srt$cumsum(dim) - 1
+  sorted_input <- input$sort(dim = dim, descending = TRUE)[[1]]
+  input_cumsum <- sorted_input$cumsum(dim) - 1
   rhos <- .make_ix_like(input, dim)
-  support <- rhos * input_srt > input_cumsum
+  support <- rhos * sorted_input > input_cumsum
 
   support_size <- support$sum(dim=dim)$unsqueeze(dim)
   tau <- input_cumsum$gather(dim, support_size)
@@ -64,17 +74,17 @@ sparsemax <- torch::nn_module(
 
 
 .entmax_threshold_and_support <- function(input, dim) {
-  input_srt <- torch::torch_sort(input, descending=TRUE, dim=dim)[[1]]
+  sorted_input <- input$sort(dim = dim, descending = TRUE)[[1]]
   rho <- .make_ix_like(input, dim)
-  mean <- input_srt$cumsum(dim) / rho
-  mean_sq <- (input_srt ^ 2)$cumsum(dim) / rho
-  ss <- rho * (mean_sq -mean ^ 2)
+  mean <- sorted_input$cumsum(dim) / rho
+  mean_sq <- sorted_input$pow(2)$cumsum(dim) / rho
+  ss <- rho * (mean_sq - mean$pow(2))
   delta <- (1 - ss) / rho
 
   delta_nz <- torch::torch_clamp(delta, 0)
   tau <- mean - torch::torch_sqrt(delta_nz)
 
-  support_size <- (tau <= input_srt)$sum(dim)$unsqueeze(dim)
+  support_size <- (tau <= sorted_input)$sum(dim)$unsqueeze(dim)
   tau_star <- tau$gather(dim, support_size)
   list(
     tau_star,
@@ -122,5 +132,78 @@ entmax <- torch::nn_module(
       self$dim <- input$dim()
     }
     entmax_function(input, self$dim)
+  }
+)
+
+#' Optimal threshold (tau) computation for 1.5-entmax
+#'
+#' @param input The input tensor to compute thresholds over.
+#' @param dim The dimension along which to apply 1.5-entmax. Default is -1.
+#' @param k The number of largest elements to partial-sort over. For optimal
+#'   performance, should be slightly bigger than the expected number of
+#'   nonzeros in the solution. If the solution is more than k-sparse,
+#'   this function is recursively called with a 2*k schedule. If `NULL`, 
+#'   full sorting is performed from the beginning. Default is NULL.
+#'
+#' @return The threshold value for each vector, with all but the `dim` 
+#'   dimension intact.
+#' @export
+get_tau <- function(input, dim = -1, k = NULL) {
+  tau_ <- ss_ <- NULL # avoid note 
+  if (is.null(k) || k >= input$dim()[dim]) { # do full sort
+    sorted_input <- input$sort(dim = dim, descending = TRUE)[[1]]
+  } else {
+    sorted_input <- input$topk(k = k, dim = dim)[[1]]
+  }
+  
+  rho <- .make_ix_like(sorted_input, dim)
+  mean <- sorted_input$cumsum(dim) / rho
+  mean_sq <- sorted_input$pow(2)$cumsum(dim) / rho
+  ss <- rho * (mean_sq - mean$pow(2))
+  delta <- (1 - ss) / rho
+  
+  # NOTE this is not exactly the same as in reference algo
+  # Fortunately it seems the clamped values never wrongly
+  # get selected by tau <= sorted_z. Prove this!
+  delta_nz <- torch::torch_clamp(delta, 0)
+  tau <- mean - delta_nz$sqrt()
+  
+  support_size <- (tau <= sorted_input)$sum(dim)$unsqueeze(dim)
+  tau_star <-  tau$gather(dim, support_size - 1)
+  
+  if (!is.null(k) && k < input$shape[dim]) {
+    unsolved <- (support_size == k)$squeeze(dim)
+    
+    if (unsolved$sum() > 0) {
+      input_ <- .roll_last(input, dim)[unsolved]
+      c(tau_, ss_) %<-% .entmax_threshold_and_support(input_, dim = -1, k = 2 * k)
+      .roll_last(tau_star, dim)[unsolved] <- tau_
+      .roll_last(support_size, dim)[unsolved] <- ss_
+    }
+  }  
+  return(tau_star)
+}
+
+relu15_function <- torch::autograd_function(
+  forward = function(ctx, X, dim = 0, tau = 0) {
+    logit <- torch::torch_clamp(X / 2 - tau, min = 0)
+    ctx$save_for_backward(logit)
+    Y <- logit$pow(2)
+    return(Y)
+  },
+  backward = function(ctx, dY) {
+    logit <- ctx$get_saved_tensors()
+    return(list(logit, NULL, NULL))
+  }
+)
+
+relu15 <- torch::nn_module(
+  "relu15",
+  initialize = function(dim = -1, tau = 0.0) {
+    self$dim <- dim
+    self$tau <- tau
+  },
+  forward = function(input) {
+    return(relu15_function$apply(input, self$dim, self$tau))
   }
 )
